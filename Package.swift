cfa6131f7d84bd30f013bd9b8f2dd7e3124d6025
Jonathan Guthrie <jono@perfect.org>
2017-01-05 10:38:46 -0500

    removed http server dep as it’s already included in sessions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ let package = Package(
     name: "Perfect-Session-Memory-Demo",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 0, minor: 0),
     ]
 )
