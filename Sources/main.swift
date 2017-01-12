93c214b131319948269ec4bfd7aee2dd7304bace
Jonathan Guthrie <jono@perfect.org>
2017-01-12 16:52:48 -0500

    Corrected header

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,15 +1,15 @@
 //
 //  main.swift
-//  PerfectTemplate
+//  Perfect Session Memory Demo
 //
-//  Created by Kyle Jessup on 2015-11-05.
-//	Copyright (C) 2015 PerfectlySoft, Inc.
+//  Created by Jonathan Guthrie on 2017-01-05.
+//	Copyright (C) 2017 PerfectlySoft, Inc.
 //
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the Perfect.org open source project
 //
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Copyright (c) 2015 - 20176 PerfectlySoft Inc. and the Perfect project authors
 // Licensed under Apache License v2.0
 //
 // See http://perfect.org/licensing.html for license information
